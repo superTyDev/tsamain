32ca5d7014abd6e1e4d748679ae51a22dc652571
superTyDev <tysonm2496@mypolkschools.net>
2021-11-12 16:59:29 -0500

    added python

--- app.py
+++ app.py
@@ -4,8 +4,9 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-				return render_template('index.html', task=task)
+@app.route("/<request>")
+def index(request="index.html"):
+				return render_template('base.html', file=request, title=(request.split('.')[0].title() +  ' | Prism Concerts'))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
