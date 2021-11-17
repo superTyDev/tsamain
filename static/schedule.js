0f1737dd3d294556334f5bd62308447b52379772
superTyDev <tysonm2496@mypolkschools.net>
2021-11-17 14:30:14 -0500

    fixed expand

--- static/schedule.js
+++ static/schedule.js
@@ -1,8 +1,7 @@
 function expandUpcoming(elem) {
 	var upcoming = document.getElementById('upcoming-expand');
-	console.log(upcoming.childNodes.length);
 	if (upcoming.offsetHeight < 4800) {
-		upcoming.style.height = (parseInt(upcoming.offsetHeight) + 800).toString() + 'px';
+		upcoming.style.maxHeight = (parseInt(upcoming.offsetHeight) + 800).toString() + 'px';
 	} else {
 		elem.style.display = "none";
 	}
